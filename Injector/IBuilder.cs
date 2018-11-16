bbea42e48002e376c1e1d8b91530972f76dc26f9
Michael Sperber <m.sperber@programmersdigest.de>
2018-11-16 17:16:59 +0100

    Added interface IBuilder to make creation of stubs for the Builder easier.

--- Injector/IBuilder.cs
+++ Injector/IBuilder.cs
@@ -8,20 +8,8 @@
     /// the mechanisms of the <see cref="DIContainer"/>.
     /// </summary>
     /// <typeparam name="TContract">The contract this builder may instantiate instances for.</typeparam>
-    public class Builder<TContract> : IBuilder<TContract> where TContract : class
+    public interface IBuilder<TContract> where TContract : class
     {
-        private DIContainer _diContainer;
-
-        /// <summary>
-        /// Creates a new instance of <see cref="Builder{TContract}"/> using the given
-        /// <paramref name="diContainer"/> for instance creation.
-        /// </summary>
-        /// <param name="diContainer">The <see cref="DIContainer"/> to use.</param>
-        public Builder(DIContainer diContainer)
-        {
-            _diContainer = diContainer;
-        }
-
         /// <summary>
         /// Creates an instance of <typeparamref name="TContract"/> using the
         /// <see cref="DIContainer"/>.
@@ -31,9 +19,6 @@
         /// In case no registration can be found for <typeparamref name="TContract"/> or the registration
         /// is invalid.
         /// </exception>
-        public TContract Build()
-        {
-            return _diContainer.Get<TContract>();
-        }
+        TContract Build();
     }
 }
